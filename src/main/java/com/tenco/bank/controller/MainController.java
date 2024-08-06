836efb7efb397aa4b8015fc6685efdfba475f839
HyeonProG <guswo3443@naver.com>
2024-08-06 11:14:42 +0900

    exception 처리 완료

--- src/main/java/com/tenco/bank/controller/MainController.java
+++ src/main/java/com/tenco/bank/controller/MainController.java
@@ -1,8 +1,14 @@
 package com.tenco.bank.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.tenco.bank.handler.exception.DataDeliveryException;
+import com.tenco.bank.handler.exception.RedirectException;
+import com.tenco.bank.handler.exception.UnAuthorizedException;
 
 @Controller  // IoC 대상(싱글톤 패턴으로 관리가 된다) -- 제어의 역전 개념
 // @ResponseBody
@@ -17,11 +23,41 @@ public class MainController {
 	public String mainPage() {
 		System.out.println("mainPage() 호출 확인");
 		// [JSP 파일 찾기 (yml 설정)] - 뷰 리졸버
-		// prefix: /WEB-INF/view
+		// prefix: /WEB-INF/view/
 		// 		   /main
 		// suffix: .jsp
 		
-		return "/main";
+		return "main";
+	}
+	
+	// TODO - 삭제 예정
+	// 주소설계
+	// http://localhost:8080/error-test1
+	
+	@GetMapping("/error-test1")
+	public String errorPage() {
+		if(true) {
+			throw new RedirectException("잘못된 요청입니다.", HttpStatus.NOT_FOUND);
+		}
+		return "main";
+	} 
+	
+	
+	// http://localhost:8080/error-test2
+	@GetMapping("/error-test2")
+	public String errorData2() {
+		if(true) {
+			throw new DataDeliveryException("잘못된 데이터 입니다", HttpStatus.BAD_REQUEST);
+		}
+		return "main";
+	} 
+	
+	@GetMapping("/error-test3")
+	public String errorData3() {
+		if(true) {
+			throw new UnAuthorizedException("인증 안된 사용자 입니다.", HttpStatus.UNAUTHORIZED);
+		}
+		return "main";
 	}
 	
 }
