8f5a2caa40372122897b21367c6cb8adc51b29da
HyeonProG <guswo3443@naver.com>
2024-08-07 11:17:27 +0900

    로그인 및 로그아웃 기능 추가

--- src/main/java/com/tenco/bank/controller/UserController.java
+++ src/main/java/com/tenco/bank/controller/UserController.java
@@ -7,38 +7,44 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.tenco.bank.dto.SignInDTO;
 import com.tenco.bank.dto.SignUpDTO;
 import com.tenco.bank.handler.exception.DataDeliveryException;
+import com.tenco.bank.repository.model.User;
 import com.tenco.bank.service.UserService;
+
+import jakarta.servlet.http.HttpSession;
 
 @Controller // IoC의 대상(싱글톤 패턴으로 관리된다)
 @RequestMapping("/user") // 대문 처리(비유적 표현)
 public class UserController {
 
 	private UserService userService;
-	
+	private final HttpSession session;
+
 	@Autowired // 노란색 경고는 사용할 필요 없음 - 가독성을 위해 선언해도 됨
-	public UserController(UserService userService) {
+	public UserController(UserService userService, HttpSession session) {
 		this.userService = userService;
+		this.session = session;
 	}
-	
+
 	/**
-	 * 회원 가입 페이지 요청
-	 * 주소 설계 : http://localhost:8080/user/sign-up
+	 * 회원 가입 페이지 요청 주소 설계 : http://localhost:8080/user/sign-up
+	 * 
 	 * @return signUp.jsp
 	 */
 	@GetMapping("/sign-up")
 	public String signUpPage() {
-		
+
 		// prefix : /WEB-INF/view/
 		// return : /user/signUp
 		// suffix : .jsp
 		return "user/signUp";
 	}
-	
+
 	/**
-	 * 회원 가입 로직 처리 요청
-	 * 주소 설계 : http://locahost:8080/user/sign-up
+	 * 회원 가입 로직 처리 요청 주소 설계 : http://locahost:8080/user/sign-up
+	 * 
 	 * @param dto
 	 * @return
 	 */
@@ -55,16 +61,66 @@ public class UserController {
 		if (dto.getPassword() == null || dto.getPassword().isEmpty()) {
 			throw new DataDeliveryException("password 를 입력 하세요", HttpStatus.BAD_REQUEST);
 		}
-		
+
 		if (dto.getFullname() == null || dto.getFullname().isEmpty()) {
 			throw new DataDeliveryException("fullname 을 입력 하세요", HttpStatus.BAD_REQUEST);
 		}
-		
+
 		// 서비스 객체로 전달
 		userService.createUser(dto);
+
+		return "redirect:/user/sign-in";
+	}
+
+	/**
+	 * 로그인 화면 요청 
+	 * 주소 설계 : http://localhost:8080/user/sign-in
+	 * 
+	 * @return
+	 */
+	@GetMapping("/sign-in")
+	public String signInPage() {
+		// 인증 검사, 유효성검사 x
+		return "user/signIn";
+	}
+
+	/**
+	 * 로그인 요청 처리
+	 * 주소 설계 : http://localhost:8080/user/sign-in
+	 * @return
+	 */
+	@PostMapping("/sign-in")
+	public String signInProc(SignInDTO dto) {
+		// 1. 인증검사 x
+		// 2. 유효성 검사 o
+		if (dto.getUsername() == null || dto.getUsername().isEmpty()) {
+			throw new DataDeliveryException("username을 입력하세요", HttpStatus.BAD_REQUEST);
+		}
 		
-		// TODO - 추후 수정
+		if (dto.getPassword() == null || dto.getPassword().isEmpty()) {
+			throw new DataDeliveryException("password를 입력하세요", HttpStatus.BAD_REQUEST);
+		}
+		
+		// 서비스 호출
+		User principal = userService.readUser(dto);
+		
+		// 세션 메모리에 등록 처리
+		session.setAttribute("principal", principal);
+		
+		// 새로운 페이지로 이동 처리
+		// TODO - 계좌 목록 페이지 이동 처리 예정
 		return "redirect:/index";
 	}
 	
+	/**
+	 * 로그아웃 기능
+	 * @return
+	 */
+	@GetMapping("/logout")
+	public String logout() {
+		session.invalidate(); // 로그아웃 됨
+		
+		return "redirect:/user/sign-in";
+	}
+
 }
--- src/main/java/com/tenco/bank/service/UserService.java
+++ src/main/java/com/tenco/bank/service/UserService.java
@@ -6,34 +6,36 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.tenco.bank.dto.SignInDTO;
 import com.tenco.bank.dto.SignUpDTO;
 import com.tenco.bank.handler.exception.DataDeliveryException;
 import com.tenco.bank.handler.exception.RedirectException;
 import com.tenco.bank.repository.interfaces.UserRepository;
+import com.tenco.bank.repository.model.User;
 
 @Service // IoC 대상(싱글톤으로 관리) - 제어의 역전
 public class UserService {
 
 	private UserRepository userRepository;
-	
+
 //	@Autowired 어노테이션으로 대체 가능
 //	생성자 의존 주입 - DI
-	
+
 	@Autowired
 	public UserService(UserRepository userRepository) {
 		this.userRepository = userRepository;
 	}
 
 	/**
-	 * 회원 등록 서비스 기능
-	 * 트랜잭션 처리
+	 * 회원 등록 서비스 기능 트랜잭션 처리
+	 * 
 	 * @param dto
 	 */
-	
+
 	@Transactional // 트랜잭션 처리는 반드시 습관화
 	public void createUser(SignUpDTO dto) {
 		int result = 0;
-		
+
 		try {
 			result = userRepository.insert(dto.toUser());
 		} catch (DataAccessException e) {
@@ -41,11 +43,29 @@ public class UserService {
 		} catch (Exception e) {
 			throw new RedirectException("알 수 없는 오류", HttpStatus.SERVICE_UNAVAILABLE);
 		}
-		
+
 		if (result != 1) {
 			throw new DataDeliveryException("회원가입 실패", HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 
+	}
+
+	public User readUser(SignInDTO dto) {
+		// 유효성 검사는 Controller에서 먼저 하자.
+		User userEntity = null; // 지역 변수 선언
+		try {
+			userEntity = userRepository.findByUsernameAndPassword(dto.getUsername(), dto.getPassword());
+		} catch (DataAccessException e) {
+			throw new DataDeliveryException("잘못된 처리 입니다.", HttpStatus.INTERNAL_SERVER_ERROR);
+		} catch (Exception e) {
+			throw new RedirectException("알 수 없는 오류", HttpStatus.SERVICE_UNAVAILABLE);
+		}
+
+		if (userEntity == null) {
+			throw new DataDeliveryException("아이디 혹은 비밀번호가 틀렸습니다.", HttpStatus.BAD_REQUEST);
+		}
+
+		return userEntity;
 	}
 
 }
